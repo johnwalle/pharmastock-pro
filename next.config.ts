4a4d925c1be8973de441b055ee3d50cc0c0039a2
mark133227 <etawe1627@gmail.com>
2025-12-16 19:00:37 +0300

    new image sources added to the config domain

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['t4.ftcdn.net', 't3.ftcdn.net', 'res.cloudinary.com'], // Add any other image domains you're using here
+    domains: ['t4.ftcdn.net', 't3.ftcdn.net', 'res.cloudinary.com', 'img.freepik.com', 'randomuser.me'], // Add any other image domains you're using here
   },
 };
 
